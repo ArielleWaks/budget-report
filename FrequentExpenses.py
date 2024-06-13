fb22b169c0816fbbee7c491475a056c2cd63bc7f
ArielleWaks <ariellemwaks@gmail.com>
2024-06-13 13:21:13 -0500

    read in spending data and display the categories w/the most purchases in bar graph

---
+++ FrequentExpenses.py
@@ -0,0 +1,32 @@
+from budget import Expense
+import collections
+import matplotlib.pyplot as plt
+
+expenses = Expense.Expenses()
+
+expenses.read_expenses('data/spending_data.csv')
+
+
+spending_categories = []
+
+for expense in expenses.list:
+    spending_categories.append(expense.category)
+
+# count categories frequencies
+spending_counter = collections.Counter(spending_categories)
+
+top5 = spending_counter.most_common(5)
+
+# convert top5 into two lists 
+categories, count = zip(*top5)
+
+# fig as Figure top level container for graph
+# ax as Axes containing figure elements
+fig, ax = plt.subplots()
+
+# bar charts with category counts
+ax.bar(categories, count)
+ax.set_title('# of Purchases by Category')
+
+# display graph
+plt.show()
